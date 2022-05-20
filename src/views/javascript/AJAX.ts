31c1472a1f9235f818d1f2ff44f6f6ee39a0b8d5
Rayan <ahmadhossiny@outlook.com>
2022-05-21 01:56:01 +0430

    ajax json + run js bookmarks

--- src/views/javascript/AJAX.ts
+++ src/views/javascript/AJAX.ts
@@ -57,6 +57,40 @@ xhttp.send();`,
             },
         ],
     },
+    {
+        title: 'ارسال و دریافت اطلاعات با فرمت جیسون',
+        text: [
+            {
+                title: ` با متد POST`,
+                code: `var xhr = new XMLHttpRequest();
+var url = "url";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.email + ", " + json.password);
+    }
+};
+var data = JSON.stringify({"email": "hey@mail.com", "password": "101010"});
+xhr.send(data);`,
+            },
+            {
+                title: ` با متد GET`,
+                code: `var xhr = new XMLHttpRequest();
+var url = "url?data=" + encodeURIComponent(JSON.stringify({"email": "hey@mail.com", "password": "101010"}));
+xhr.open("GET", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.email + ", " + json.password);
+    }
+};
+xhr.send();`,
+            },
+        ],
+    },
 ];
 
 export default AJAX;
--- src/views/javascript/Run.vue
+++ src/views/javascript/Run.vue
@@ -301,6 +301,70 @@
           </div>
           <!--end::Section-->
 
+          <!--begin::Section-->
+          <div class="m-0">
+            <!--begin::Heading-->
+            <div
+                class="
+                d-flex
+                align-items-center
+                collapsible
+                py-3
+                toggle
+                mb-0
+                collapsed
+              "
+                data-bs-toggle="collapse"
+                data-bs-target="#kt_run_5"
+                aria-expanded="false"
+            >
+              <!--begin::Icon-->
+              <div
+                  class="
+                  btn btn-sm btn-icon
+                  mw-20px
+                  btn-active-color-primary
+                  me-5
+                "
+              >
+                <span class="svg-icon toggle-on svg-icon-primary svg-icon-1">
+                  <inline-svg :src="require('@/assets/media/icons/gen036.svg')" />
+                </span>
+                <span class="svg-icon toggle-off svg-icon-1">
+                  <inline-svg :src="require('@/assets/media/icons/gen035.svg')" />
+                </span>
+              </div>
+              <!--end::Icon-->
+
+              <!--begin::Title-->
+              <h4 class="text-gray-700 fw-bolder cursor-pointer mb-0">
+                داخل Bookmark های مرورگر
+              </h4>
+              <!--end::Title-->
+            </div>
+            <!--end::Heading-->
+
+            <!--begin::Body-->
+            <div id="kt_run_5" class="fs-6 ms-1 collapse">
+              <!--begin::Text-->
+              <div class="mb-4 text-gray-600 fw-bold fs-6 ps-10">
+                در این حالت کد جاوا اسکریپت به عنوان یک لینک ذخیره می شود. با این تفاوت که ابتدای کد جاوا اسکریپت باید عبارت javascript: نوشته شود.
+                <div class="pb-5">
+                  <CodeHighlighter lang="javascript"
+                  >{{ `javascript:alert('Salam');` }}
+                  </CodeHighlighter>
+                </div>
+              </div>
+              <!--end::Text-->
+            </div>
+            <!--end::Content-->
+
+            <!--begin::Separator-->
+            <div class="separator separator-dashed"></div>
+            <!--end::Separator-->
+          </div>
+          <!--end::Section-->
+
           <!--end::Accordion-->
         </div>
         <!--end::Col-->
